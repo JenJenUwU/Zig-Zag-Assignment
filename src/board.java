df8165177ff424e8f0942111729268bc27107d47
Jay <jenjentw358@gmail.com>
2023-11-20 23:18:55 -0500

    reverse string for odd number rows

--- src/board.java
+++ src/board.java
@@ -1,6 +1,6 @@
 public class board {
-    private int boardWidth;
-    private int boardHeight;
+    private final int boardWidth;
+    private final int boardHeight;
     private String[][] board;
 
     public board(int width, int height) {
@@ -10,25 +10,37 @@ public class board {
         int numSpaces = width * height;
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (i % 2 == 0) {
-                    board[i][j] = String.valueOf(i * boardHeight + j);
-                } else {
-                    board[i][j] = String.valueOf(i * boardHeight + (width - j - 1));
-                }
+                board[i][j] = String.valueOf(i * boardWidth + j + 1);
             }
         }
+    }
+
+    private String formatSpace(String item) {
+        while (item.length() < String.valueOf(boardWidth * boardHeight).length()) {
+            item = " " + item;
+        }
+        return item;
+    }
+
+    public int[] getCoordinates(int space) {
+        int[] coordinates = new int[2];
+        coordinates[1] = (space - 1) % boardWidth;//y
+        coordinates[0] = (space - 1) / boardWidth;//x
+        return coordinates;
     }
 
     @Override
     public String toString() {
         String returnString = "";
-        for (String arr[] : board) {
-            for (int i = 0; i < arr.length; i++) {
-                String temp = arr[i];
-                while (temp.length() < String.valueOf(boardWidth * boardHeight).length() || temp.length() < 2) {
-                    temp = " " + temp;
+        for (int col = 0; col < boardHeight; col++) {
+            for (int row = 0; row < boardWidth; row++) {
+                String temp;
+                if (col % 2 == 0) {
+                    temp = formatSpace(board[col][row]);
+                } else {
+                    temp = formatSpace(board[col][boardWidth - row - 1]);
                 }
-                if (i != arr.length - 1) {
+                if (row != board[col].length - 1) {
                     returnString += temp + " ";
                 } else {
                     returnString += temp + "\n";
--- src/game.java
+++ src/game.java
@@ -1,6 +1,7 @@
 public class game {
     public static void main(String[] args) {
-        board board = new board(3, 5);
+        board board = new board(6, 5);
         System.out.println(board);
+        System.out.println(board.getCoordinates(3)[0]);
     }
 }
